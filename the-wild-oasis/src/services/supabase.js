c8dd4f879bd3a45c81ee7ddbf2b73cdfa4d4cf75
Dimitar Damianov <114077179+dimitargb@users.noreply.github.com>
2024-09-21 15:12:47 +0300

    I no longer need this direcory

--- the-wild-oasis/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "https://kgxfrgapcuscqhwuxywn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtneGZyZ2FwY3VzY3Fod3V4eXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyNDc5MzgsImV4cCI6MjAzOTgyMzkzOH0.aToKhAEE6ZdJQClShuKR1XtRmcyZlpIQLJJH7RZ0zY0";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
